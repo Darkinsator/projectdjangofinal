99331f5c9a72b82c0d2d20128d8f9345b2ebdce1
Darkinsator <jasonblankenberg02@gmail.com>
2025-01-24 14:43:56 +0200

    horhrorhrorohrhorohrohro

--- budget/urls.py
+++ budget/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -8,3 +10,7 @@ urlpatterns = [
     path('add', views.ProjectCreateView.as_view(), name='add'),
     path('<slug:project_slug>', views.project_detail, name='detail')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
