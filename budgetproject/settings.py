fc4cc810c21b59afabf9306d03b7e5bf2dec7ce5
Darkinsator <jasonblankenberg02@gmail.com>
2025-01-24 12:36:08 +0200

    done

--- budgetproject/settings.py
+++ budgetproject/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
